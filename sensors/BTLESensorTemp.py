5fc8cf2e64bc9be9cafece3242eaa0569d7d32b8
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-01-29 18:22:15 -0500

    temp sensor fixed num readings

--- sensors/BTLESensorTemp.py
+++ sensors/BTLESensorTemp.py
@@ -590,7 +590,8 @@ class BTSensorTemp():
 
             # Update the device status dict
             self.results_dict['status'] = 'Reading'
-            self.results_dict['data']['temp'] = temp   
+            self.results_dict['data']['temp_celcius'] = temp   
+            self.results_dict['data']['temp_farenheit'] = temp*1.8 +32   
             self.results_dict['timestamp'] = time_stamp
             self.results_dict['data']['good_readings'] = self.good_readings
             self.results_dict['data']['total_readings'] = self.number_readings
